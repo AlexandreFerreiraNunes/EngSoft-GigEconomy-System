7329762cfd3d7ce507207a48412fadc72e2a60f4
09Ghost90 <brunosousapereira.az@gmail.com>
2026-03-05 16:30:12 -0300

    TimeLife Acess Token atualizado

--- config/settings.py
+++ config/settings.py
@@ -152,7 +152,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=300),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=365),
 
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
